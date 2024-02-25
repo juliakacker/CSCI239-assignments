f81f027dc7befe198d91fcb1c6a8f33260ad4f08
Juju Acker <161252404+juliakacker@users.noreply.github.com>
2024-02-25 16:36:21 -0500

    Update Ch7_PrExercise1.java

--- Ch7_PrExercise1.java
+++ Ch7_PrExercise1.java
@@ -1,3 +1,5 @@
+//Write a value-returning method, isVowel, that returns the value true if a given character is a vowel, and otherwise returns false. Also write a program to test your method.
+
 package Ch7_PrExercise1;
 
 import java.lang.*;
