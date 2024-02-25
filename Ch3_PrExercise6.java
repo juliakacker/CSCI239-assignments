009989c2b55c3d066277985fe57caea4c7806f31
Juju Acker <161252404+juliakacker@users.noreply.github.com>
2024-02-25 16:30:10 -0500

    Update Ch3_PrExercise6.java

--- Ch3_PrExercise6.java
+++ Ch3_PrExercise6.java
@@ -1,3 +1,21 @@
+/* Write a program that calculates and prints the monthly paycheck for an employee. The net pay is calculated after taking the following deductions:
+Federal Income Tax: 15%
+State Tax: 3.5%
+Social Security Tax: 5.75%
+Medicare/Medicaid Tax: 2.75%
+Pension Plan: 5%
+Health Insurance: $75.00
+Your program should prompt the user to input the gross amount and the employee name. The output will be stored in a file. Format your output to have two decimal places. A sample output follows:
+Bill Robinson
+Gross Amount: $ 3575.00
+Federal Tax: $ 536.25
+State Tax: $ 125.13
+Social Security Tax: $ 205.56
+Medicare/Medicaid Tax: $ 98.31
+Pension Plan: $ 178.75
+Health Insurance: $ 75.00
+Net Pay: $ 2356.00 */
+
 package Ch3_PrExercise6;
 import java.util.*;
 import java.io.*;
