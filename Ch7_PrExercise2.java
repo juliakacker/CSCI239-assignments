9d5a5ec313d07b5bde47d1d932517c424218b737
Juju Acker <161252404+juliakacker@users.noreply.github.com>
2024-02-25 16:38:10 -0500

    Update Ch7_PrExercise2.java

--- Ch7_PrExercise2.java
+++ Ch7_PrExercise2.java
@@ -1,3 +1,5 @@
+//Write a program that prompts the user to input a sequence of characters and outputs the number of vowels. (Use the method isVowel written in Programming Exercise 1.)
+
 package Ch7_PrExercise2;
 
 import java.lang.*;
