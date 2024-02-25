21b997d3c947b78347700e87bbdcf600aae0023b
Juju Acker <161252404+juliakacker@users.noreply.github.com>
2024-02-25 16:20:58 -0500

    Update Ch5_PrExercise1.java

--- Ch5_PrExercise1.java
+++ Ch5_PrExercise1.java
@@ -1,10 +1,4 @@
-/* Write a program that prompts the user to input an integer and then outputs both the 
-individual digits of the number and the sum of the digits. For example, 
-the program should: output the individual digits of 3456 as 3 4 5 6 and the sum as 18, 
-output the individual digits of 8030 as 8 0 3 0 and the sum as 11, 
-output the individual digits of 2345526 as 2 3 4 5 5 2 6 and the sum as 27, 
-output the individual digits of 4000 as 4 0 0 0 and the sum as 4, 
-and output the individual digits of -2345 as 2 3 4 5 and the sum as 14.*/
+//Write a program that prompts the user to input an integer and then outputs both the individual digits of the number and the sum of the digits. For example, the program should: output the individual digits of 3456 as 3 4 5 6 and the sum as 18, output the individual digits of 8030 as 8 0 3 0 and the sum as 11, output the individual digits of 2345526 as 2 3 4 5 5 2 6 and the sum as 27, output the individual digits of 4000 as 4 0 0 0 and the sum as 4, and output the individual digits of -2345 as 2 3 4 5 and the sum as 14.
 
 package Ch5_PrExercise1;
 import java.util.*;
