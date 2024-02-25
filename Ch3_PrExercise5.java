93ef2bd05fa4e359bc52bea43b3bc0d31fb9cae8
Juju Acker <161252404+juliakacker@users.noreply.github.com>
2024-02-25 16:28:20 -0500

    Update Ch3_PrExercise5.java

--- Ch3_PrExercise5.java
+++ Ch3_PrExercise5.java
@@ -1,3 +1,14 @@
+/* The manager of a football stadium wants you to write a program that calculates the total ticket sales after each game. There are four types of tickets—box, sideline, premium, and general admission. After each game, data is stored in a file in the following form:
+ticketPrice numberOfTicketsSold
+
+Sample data are shown below: 
+250 5750
+100 28000
+50 35750
+25 18750
+
+The first line indicates that the box ticket price is $250 and that 5750 tickets were sold at that price. Output the number of tickets sold and the total sale amount. Format your output with two decimal places. */
+
 package Ch3_PrExercise5;
 import java.util.*;
 import java.io.*;
