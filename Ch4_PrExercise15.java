090beb97120c38334b1fdebb5d9dc8f3a002cd15
Juju Acker <161252404+juliakacker@users.noreply.github.com>
2024-02-25 16:32:50 -0500

    Update Ch4_PrExercise15.java

--- Ch4_PrExercise15.java
+++ Ch4_PrExercise15.java
@@ -1,3 +1,13 @@
+/* Write a program that calculates and prints the bill for a cellular telephone company. The company offers two types of service: regular and premium. Rates vary based on the type of service and are computed as follows:
+
+Regular service: $10.00 plus first 50 minutes are free. Charges for over 50 minutes are $0.20 per minute.
+
+Premium service: $25.00 plus:
+a. For calls made from 6:00 a.m. to 6:00 p.m., the first 75 minutes are free; charges for over 75 minutes are $0.10 per minute.
+b. For calls made from 6:00 p.m. to 6:00 a.m., the first 100 minutes are free; charges for over 100 minutes are $0.05 per minute.
+
+Your program should prompt the user to enter an account number, a service code (typechar), and the number of minutes the service was used. A service code of r or R means regular service; a service code of p or P means premium service. Treat any other character as an error. Your program should output the account number, type of service, number of minutes the telephone service was used, and the amount due from the user. For the premium service, the customer may be using the service during the day and the night. Therefore, to calculate the bill, you must ask the user to input the number of minutes the service was used during the day and the number of minutes the service was used during the night. */
+
 package Ch4_PrExercise15;
 import java.util.*;
 import java.io.*;
