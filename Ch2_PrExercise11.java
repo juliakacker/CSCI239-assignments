32aba356c066394dea919fc609b0779d156aa830
Juju Acker <161252404+juliakacker@users.noreply.github.com>
2024-02-25 16:12:32 -0500

    Update Ch2_PrExercise11.java

--- Ch2_PrExercise11.java
+++ Ch2_PrExercise11.java
@@ -1,3 +1,4 @@
+//Write a Java program that prompts the user to input the elapsed time for an event in seconds. The program then outputs the elapsed time in hours,minutes, and seconds. (For example, if the elapsed time is 9630 seconds,then the output is 2:40:30.)
 package Ch2_PrExercise11;
 import java.util.*;
 
